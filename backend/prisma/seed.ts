4f35f16cabef3f4dbecf108118383a06910e6bfc
LUCA DURANDO <s303395@studenti.polito.it>
2022-11-10 16:35:43 +0100

    Add dummy data

--- backend/prisma/seed.ts
+++ backend/prisma/seed.ts
@@ -1,86 +1,76 @@
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient, Prisma } from '@prisma/client'
+import { faker } from "@faker-js/faker/locale/it"
 const prisma = new PrismaClient()
 
-async function main() {
-    const user1 = {
-        type: 'Hiker',
-        email: 'prova@hiker.io',
-        username: 'Utente Prova',
-        phoneNumber : '3297898112',
-        performance: {
-          create: {
-            duration: 100,
-            altitude: 100.7
-          },
-        },
+const regioni = [
+  "Abruzzo",
+  "Basilicata",
+  "Calabria",
+  "Campania",
+  "Emilia-Romagna",
+  "Friuli-Venezia Giulia",
+  "Lazio",
+  "Liguria",
+  "Lombardia",
+  "Marche",
+  "Molise",
+  "Piemonte",
+  "Puglia",
+  "Sardegna",
+  "Sicilia",
+  "Toscana",
+  "Trentino-Alto Adige",
+  "Umbria",
+  "Valle d'Aosta",
+  "Veneto"
+]
+
+const province = ["Agrigento", "Alessandria", "Ancona", "Arezzo", "AscoliPiceno", "Asti", "Avellino", "Bari", "Barletta-Andria-Trani", "Belluno", "Benevento", "Bergamo", "Biella", "Bologna", "Bolzano", "Brescia", "Brindisi", "Cagliari", "Caltanissetta", "Campobasso", "Caserta", "Catania", "Catanzaro", "Chieti", "Como", "Cosenza", "Cremona", "Crotone", "Cuneo", "Enna", "Fermo", "Ferrara", "Firenze", "Foggia", "Forlì-Cesena", "Frosinone", "Genova", "Gorizia", "Grosseto", "Imperia", "Isernia", "L'Aquila", "LaSpezia", "Latina", "Lecce", "Lecco", "Livorno", "Lodi", "Lucca", "Macerata", "Mantova", "Massa-Carrara", "Matera", "Messina", "Milano", "Modena", "MonzaedellaBrianza", "Napoli", "Novara", "Nuoro", "Oristano", "Padova", "Palermo", "Parma", "Pavia", "Perugia", "PesaroeUrbino", "Pescara", "Piacenza", "Pisa", "Pistoia", "Pordenone", "Potenza", "Prato", "Ragusa", "Ravenna", "ReggioCalabria", "Reggionell'Emilia", "Rieti", "Rimini", "Roma", "Rovigo", "Salerno", "Sassari", "Savona", "Siena", "Siracusa", "Sondrio", "SudSardegna", "Taranto", "Teramo", "Terni", "Torino", "Trapani", "Trento", "Treviso", "Trieste", "Udine", "Valled'Aosta", "Varese", "Venezia", "Verbano-Cusio-Ossola", "Vercelli", "Verona", "ViboValentia", "Vicenza", "Viterbo"]
+
+const hikers: Prisma.UserCreateInput[] = Array(10).fill(0).map(e => {
+  return {
+    type: 'hiker',
+    username: faker.internet.userName(),
+    email: faker.internet.email(),
+    phoneNumber: faker.phone.number(),
+  }
+})
+
+const guides: Prisma.UserCreateInput[] = Array(10).fill(0).map(e => {
+  return {
+    type: 'guide',
+    username: faker.internet.userName(),
+    email: faker.internet.email(),
+    phoneNumber: faker.phone.number(),
+  }
+})
+
+const hikes: Prisma.HikeCreateInput[] = Array(20).fill(0).map(e => {
+  return {
+    Title: faker.commerce.productName(),
+    Description: faker.lorem.paragraph(),
+    Ascent: Math.floor(Math.random() * 1000) - 500,
+    Difficulty: faker.random.numeric(5),
+    Length: Math.floor(Math.random() * 10000),
+    Expected_time: Math.floor(Math.random() * 500),
+    Start_point: {
+      create: {
+        City: faker.address.city(),
+        Region: regioni[Math.floor(Math.random() * regioni.length)],
+        Province: province[Math.floor(Math.random() * province.length)],
       }
-
-      const user2 = {
-        type: 'Hut Worker',
-        email: 'butter@hutter.io',
-        username: 'Peanut Butter',
-        phoneNumber : '3297898321',
-        performance: {
-          create: {
-            duration: 50,
-            altitude: 12.7
-          },
-        },
+    },
+    End_point: {
+      create: {
+        City: faker.address.city(),
+        Region: regioni[Math.floor(Math.random() * regioni.length)],
+        Province: province[Math.floor(Math.random() * province.length)],
       }
+    },
 
+  }
+})
 
-    const hike1 = {
-        Title: 'Sentiero nei Boschi',
-        Length: 30.2,
-        Expected_time: 180,
-        Ascent : 0.2,
-        Difficulty: 'hard',
-        Start_point: {
-          create: {
-            Label: 'hut',
-            Latitude: 22.7,
-            Longitude: 45.8,
-            Elevation : 600
-          },
-        },
-        End_point: {
-            create: {
-              Label: 'parking lot',
-              Latitude: 44.7,
-              Longitude: 40.4,
-              Elevation : 600
-            },
-        },
-        Reference_points: {
-            create: [{
-                Label: 'first important point',
-                Latitude: 29.7,
-                Longitude: 30.4,
-                Elevation : 600
-            },
-            {
-                Label: 'second important point',
-                Latitude: 29.7,
-                Longitude: 30.4,
-                Elevation : 600
-            },
-        ]},
-        Description: 'fun and interactive'
-
-
-    }
-   
-     await prisma.user.create({ data: user1 })
-     await prisma.user.create({ data: user2 })
-     await prisma.hike.create({ data: hike1 })
-   
-}
-main()
-  .then(async () => {
-    await prisma.$disconnect()
-  })
-  .catch(async (e) => {
-    console.error(e)
-    await prisma.$disconnect()
-    process.exit(1)
-  })
+const userPromises = hikers.concat(guides).map(u => prisma.user.create({ data: u }))
+const hikesPromises = hikes.map(h => prisma.hike.create({ data: h }))
+Promise.all([...userPromises, ...hikesPromises])
