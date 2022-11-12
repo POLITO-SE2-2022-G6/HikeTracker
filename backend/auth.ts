6f11d46c03d6a1532015645c164f11eb71ad83ca
LUCA DURANDO <s303395@studenti.polito.it>
2022-11-12 22:59:32 +0100

    [backend] authentication

---
+++ backend/auth.ts
@@ -0,0 +1,19 @@
+import { PrismaClient } from '@prisma/client';
+const prisma = new PrismaClient();
+
+export async function getUserById(id:number) {
+    return await prisma.user.findUnique({ where: { id: id } })
+}
+
+export async function getUserByEmail(email:string) {
+    return await prisma.user.findUnique({ where: { email: email } })
+}
+
+export async function createUsr(type: string, username: string, email: string, phoneNumber: string){
+    await prisma.user.create({data: {
+        type:type,
+        username: username,
+        email:email,
+        phoneNumber:phoneNumber}
+    })
+}
--- backend/index.ts
+++ backend/index.ts
@@ -1,14 +1,114 @@
-import express from "express";
-import { validationResult, query, body } from 'express-validator';
+import express, { RequestHandler } from "express";
+import passport from "passport";
+import passportLocal from "passport-local";
+import session from "express-session";
+import { validationResult, query, body, checkSchema } from 'express-validator';
 import { hikesList, createHike, editHike } from "./visitorDao"
 import cors from "cors";
+import { getUserById, getUserByEmail, createUsr } from "./auth";
 
 const app = express();
 const port = 3001;
+const LocalStrategy = passportLocal.Strategy;
+
+export type User = {
+  id: number,
+  email: string,
+  type: string,
+  username: string,
+  phoneNumber: string
+}
+
+passport.serializeUser<any, any>((req, user, done) => { done(undefined, user);});
+  
+passport.deserializeUser((id:number, done) => {
+  const usr = getUserById(id);
+  if (!usr) done("User not found", undefined);
+  else done(undefined, usr);   
+});
+
+passport.use(new LocalStrategy(async (email: string, p, done) =>{
+  const usr = getUserByEmail(email);
+  if (!usr) done("User not found", undefined);
+  else done(undefined, usr);   
+}))
+
 
 app.use(cors({
     origin: "http://localhost:3000"
 }));
+
+app.use(
+  session({
+    // by default, Passport uses a MemoryStore to keep track of the sessions
+    secret:
+      "a secret sentence not to share with anybody and anywhere, used to sign the session ID cookie",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+const isLoggedIn: RequestHandler = (req, res, next) => {
+  if (req.isAuthenticated()) return next();
+
+  return res.status(401).json({ error: "not authenticated" });
+};
+
+/*** Users APIs ***/
+// SignIN
+app.post("/signup", checkSchema({
+  type:{
+    in: "query",
+    isString: true
+  },
+  username:{
+    in: "query",
+    isString: true
+  },
+  email:{
+    in: "query",
+    isEmail: true
+  },
+  phoneNumber:{
+    in: "query",
+    isMobilePhone: true
+  }}), async (req:express.Request,res:express.Response) =>  {
+  const { type, username, email, phoneNumber } = req.query as Record<string, string>;
+  createUsr(type, username, email, phoneNumber).then(() => {res.end()}).catch((err) => {res.status(500).json({error: err})});
+}) 
+  
+// Login --> POST /sessions
+app.post("/api/sessions", function (req, res, next) {
+  passport.authenticate("local", (err, user, info) => {
+    if (err) return next(err);
+    if (!user) return res.status(401).json(info);
+    
+    // success, perform the login
+    req.login(user, (err) => {
+      if (err) return next(err);
+      // req.user contains the authenticated user, we send all the user info back
+      // this is coming from userDao.getUser()
+      return res.json(req.user);
+    });
+  })(req, res, next);
+});
+
+// Logout --> DELETE /sessions/current 
+app.delete("/api/sessions/current", (req, res) => {
+  req.logout(()=>{});
+  res.end();
+});
+
+// GET /sessions/current
+// check whether the user is logged in or not
+app.get("/api/sessions/current", (req , res) => {
+  if (req.isAuthenticated()) {
+    res.status(200).json(req.user);
+  } else res.status(401).json({ error: "Unauthenticated user!" });
+});
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}.`);
@@ -16,7 +116,7 @@ app.listen(port, () => {
 
 //HOME
 //Add check if logged in
-app.get("/", 
+app.get("/",
   query("city").optional().notEmpty(), query("province").optional().notEmpty(), query("region").optional().notEmpty(),
   query("difficulty").optional().notEmpty(), query("length").optional().isFloat(), query("ascent").optional().isFloat(),
   query("expected_time").optional().isFloat(),  async (req: express.Request, res: express.Response) => {
@@ -53,9 +153,9 @@ app.put("/:id",
   body("title").optional().notEmpty(), body("length").optional().notEmpty(), body("expected_time").optional().isInt(),
   body("ascent").optional().isFloat(), body("difficulty").optional().notEmpty(), body("description").optional().notEmpty(),
   body("gpstrack").optional().notEmpty(),
-  async (req: express.Request, res: express.Response) => {
+  async (req, res) => {
     if (!validationResult(req).isEmpty()) return res.status(400).json({ errors: "Illegal Data" });
-    const id: number = parseInt(req.params.id, 10);
+    const id: number = parseInt(req.params!.id, 10);
     const params = req.body;
     const modifiedHike = await editHike(id,params);
     return res.status(201).json(modifiedHike);  
