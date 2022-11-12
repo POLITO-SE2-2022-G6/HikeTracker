34f1050883a13515c5ea8b70b38906ced7c71337
LUCA DURANDO <s303395@studenti.polito.it>
2022-11-12 17:14:07 +0100

    [backend] Validators

--- backend/index.ts
+++ backend/index.ts
@@ -1,9 +1,7 @@
 import express from "express";
-const { validationResult, query } = require('express-validator');
-import { stringify } from "querystring";
+import { validationResult, query, body } from 'express-validator';
 import { hikesList, createHike, editHike } from "./visitorDao"
 import cors from "cors";
-
 
 const app = express();
 const port = 3001;
@@ -16,18 +14,12 @@ app.listen(port, () => {
   console.log(`Server is running on port ${port}.`);
 });
 
-function validateParameters(props: any){
-  return (true && (props("city").exists().isLength({min:1}) || props("city").exists({checkFalsy: true})) 
-  && (props("province").exists().isLength({min:1}) || props("province").exists({checkFalsy: true})) 
-  && (props("region").exists().isLength({min:1}) || props("region").exists({checkFalsy: true}))
-  && (props("difficulty").exists().isLength({min:1}) || props("difficulty").exists({checkFalsy: true}))
-  && (props("length").exists().isLength({min:1}) || props("difficulty").exists({checkFalsy: true}))
-  && (props("ascent").exists().isLength({min:1}) || props("ascent").exists({checkFalsy: true})) 
-  && (props("expected_time").exists().isNumeric() || props("expected_time").exists({checkFalsy: true})))
-}
 //HOME
 //Add check if logged in
-app.get("/", validateParameters(query()) , async (req: express.Request, res: express.Response) => {
+app.get("/", 
+  query("city").optional().notEmpty(), query("province").optional().notEmpty(), query("region").optional().notEmpty(),
+  query("difficulty").optional().notEmpty(), query("length").optional().isFloat(), query("ascent").optional().isFloat(),
+  query("expected_time").optional().isFloat(),  async (req: express.Request, res: express.Response) => {
   if (!validationResult(req).isEmpty()) return res.status(400).json({ errors: "Illegal Data" });
 
   const { city, province, region, difficulty, length, ascent, expected_time } = req.query as Record<string, string | undefined>;
@@ -45,30 +37,28 @@ app.get("/", validateParameters(query()) , async (req: express.Request, res: exp
 
 
 //New Hike in Body
-app.post("/", /*queryValidation*/async (req: express.Request, res: express.Response) => {
-
-  try {
+app.post("/", 
+  body("title").exists().notEmpty(), body("length").exists().notEmpty(), body("expected_time").exists().isInt(),
+  body("ascent").exists().isFloat(), body("difficulty").exists().notEmpty(), body("description").exists().notEmpty(),
+  body("gpstrack").optional().notEmpty(), 
+  async (req: express.Request, res: express.Response) => {
+    if (!validationResult(req).isEmpty()) return res.status(400).json({ errors: "Illegal Data" });
     const hike = req.body;
     const newHike = await createHike(hike);
     return res.status(201).json(newHike);
-  } catch (error: any) {
-    return res.status(500).json(error);
-  }
-  
 })
 
 //Edit Hike
-app.put("/:id", /*queryValidation*/async (req: express.Request, res: express.Response) => {
-
-  const id: number = parseInt(req.params.id, 10);
-  try {
+app.put("/:id",
+  body("title").optional().notEmpty(), body("length").optional().notEmpty(), body("expected_time").optional().isInt(),
+  body("ascent").optional().isFloat(), body("difficulty").optional().notEmpty(), body("description").optional().notEmpty(),
+  body("gpstrack").optional().notEmpty(),
+  async (req: express.Request, res: express.Response) => {
+    if (!validationResult(req).isEmpty()) return res.status(400).json({ errors: "Illegal Data" });
+    const id: number = parseInt(req.params.id, 10);
     const params = req.body;
     const modifiedHike = await editHike(id,params);
-    return res.status(201).json(modifiedHike);
-  } catch (error: any) {
-    return res.status(500).json(error);
-  }
-  
+    return res.status(201).json(modifiedHike);  
 })
 
 
