f1f5a891413931eaf6b8e18d9d28d47706fb4003
Francesco Bruno <b.frenc@hotmail.it>
2022-11-27 16:42:30 +0100

    [Frontend] Add map to Hut and Parkinglot Forms

--- frontend/src/pages/hutForm/HutForm.tsx
+++ frontend/src/pages/hutForm/HutForm.tsx
@@ -1,9 +1,13 @@
 import s from './HutForm.module.css';
 import { useForm } from '@mantine/form'
 import axios from 'axios';
-import { Button, Container, Paper, TextInput, Title, Group, Textarea } from '@mantine/core';
+import { Button, Container, Paper, TextInput, Title, Group, Textarea, Box, Text } from '@mantine/core';
 import { useNavigate, Link } from 'react-router-dom';
 import { useState } from 'react';
+import { MapContainer, Marker, TileLayer, useMapEvent } from 'react-leaflet';
+import { LatLng } from 'leaflet';
+import { API } from '../../utilities/api/api';
+import { CiLocationOn } from 'react-icons/ci';
 //import { IconUpload } from '@tabler/icons';
 //import { useParams } from 'react-router-dom';
 
@@ -11,11 +15,31 @@ const HutForm: React.FC = () => {
 
   const navigate = useNavigate();
   const [error, setError] = useState('');
+  const [marker, setMarker] = useState<LatLng>(new LatLng(0, 0));
+  const [display_name, setDisplayName] = useState('');
+
+  const choosePosition = async (pos: LatLng) => {
+    setMarker(pos);
+    const res = await axios.get(`https://nominatim.openstreetmap.org/reverse`,
+      { params: { lat: pos.lat, lon: pos.lng, format: 'json', 'accept-language': 'it', zoom: 10 } }
+    )
+    if (res.data) {
+      const { address, display_name } = res.data;
+      setDisplayName(display_name);
+      form.setValues({ city: address.city, province: address.county, region: address.state, latitude: pos.lat, longitude: pos.lng });
+    }
+
+  }
 
 
   type Fields = {
     title?: string;
     description?: string;
+    latitude?: number;
+    longitude?: number;
+    province?: string;
+    region?: string;
+    city?: string;
     //gpstrack?: File;
   }
 
@@ -23,6 +47,9 @@ const HutForm: React.FC = () => {
     initialValues: {
       title: '',
       description: '',
+      province: '',
+      region: '',
+      city: '',
       //gpstrack: undefined,
     },
 
@@ -33,7 +60,7 @@ const HutForm: React.FC = () => {
   });
 
   const handleSubmit = async (values: Fields) => {
-    
+
     addHut(values)
 
   }
@@ -41,14 +68,19 @@ const HutForm: React.FC = () => {
   const addHut = async (values: Fields) => {
     try {
       console.log(values);
-      /*const res = await axios.post('http://localhost:3001/hut', values, QUI AGGIUNGI NUOVO HUT
-        {
-          withCredentials: true,
-          headers: {
-            'Content-Type': 'multipart/form-data'
-          }
-        })*/
-         navigate('/');
+      const res = await API.point.createPoint({
+        City: values.city,
+        Latitude: values.latitude,
+        Longitude: values.longitude,
+        Province: values.province,
+        Region: values.region,
+        Label: values.title,
+        Hut: {
+          Description: values.description,
+        }
+      });
+      console.log(res);
+      navigate('/');
 
     } catch (err) {
       setError('Error - creating a new hut');
@@ -82,17 +114,70 @@ const HutForm: React.FC = () => {
               label="Description"
               placeholder="Description of the hut"
               {...form.getInputProps('description')} />
+
+            {display_name && <Paper p={'sm'} withBorder mt={'md'}>
+              <Group>
+                <CiLocationOn style={{ verticalAlign: 'middle' }} size='2em' />
+                <Text fz='lg'>{display_name}</Text>
+              </Group>
+            </Paper>}
+
+            <Box h='300px' mt={'md'}>
+              <MapContainer center={[41.3, 12.5]} zoom={9} className={s.map} >
+                <TileLayer
+                  url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                />
+                <EventHandler choosePosition={choosePosition} />
+                {marker && <Marker position={marker} />}
+              </MapContainer>
+            </Box>
+
+            <TextInput hidden
+              {...form.getInputProps('latitude')}
+            />
+
+            <TextInput hidden
+              {...form.getInputProps('longitude')}
+            />
+
+            <TextInput hidden
+              {...form.getInputProps('province')}
+            />
+
+            <TextInput hidden
+              {...form.getInputProps('region')}
+            />
+
+            <TextInput hidden
+              {...form.getInputProps('city')}
+            />
+
+
             <Group position="center">
               <Button mt="xl" type='submit'>Add Hut</Button>
               <Link to="/userarea">
                 <Button mt="xl" color="red">Cancel</Button>
               </Link>
             </Group>
+
           </form>
         </Paper>
       </Container>
     </Container>
   )
 }
+
+interface EventHandlerProps {
+  choosePosition: (marker: LatLng) => void;
+}
+
+const EventHandler = (props: EventHandlerProps) => {
+  const { choosePosition } = props;
+  const map = useMapEvent('click', (e) => {
+    choosePosition(e.latlng);
+  })
+  return null
+}
+
 
 export default HutForm;
--- frontend/src/pages/parkingLotForm/ParkingLotForm.tsx
+++ frontend/src/pages/parkingLotForm/ParkingLotForm.tsx
@@ -1,9 +1,13 @@
 import s from './ParkingLotForm.module.css';
 import { useForm } from '@mantine/form'
 import axios from 'axios';
-import { Button, Container, Paper, TextInput, Title, Group, Textarea } from '@mantine/core';
+import { Button, Container, Paper, TextInput, Title, Group, Textarea, Box, Text } from '@mantine/core';
 import { useNavigate, Link } from 'react-router-dom';
 import { useState } from 'react';
+import { LatLng } from 'leaflet';
+import { MapContainer, useMapEvent, TileLayer, Marker } from 'react-leaflet';
+import { API } from '../../utilities/api/api';
+import { CiLocationOn } from 'react-icons/ci'
 //import { IconUpload } from '@tabler/icons';
 //import { useParams } from 'react-router-dom';
 
@@ -11,11 +15,31 @@ const ParkingLotForm: React.FC = () => {
 
   const navigate = useNavigate();
   const [error, setError] = useState('');
+  const [marker, setMarker] = useState<LatLng>(new LatLng(0, 0));
+  const [display_name, setDisplayName] = useState('');
+
+  const choosePosition = async (pos: LatLng) => {
+    setMarker(pos);
+    const res = await axios.get(`https://nominatim.openstreetmap.org/reverse`,
+      { params: { lat: pos.lat, lon: pos.lng, format: 'json', 'accept-language': 'it', zoom: 10 } }
+    )
+    if (res.data) {
+      const { address, display_name } = res.data;
+      setDisplayName(display_name);
+      form.setValues({ city: address.city, province: address.county, region: address.state, latitude: pos.lat, longitude: pos.lng });
+    }
+
+  }
 
 
   type Fields = {
     title?: string;
     description?: string;
+    latitude?: number;
+    longitude?: number;
+    province?: string;
+    region?: string;
+    city?: string;
     //gpstrack?: File;
   }
 
@@ -23,6 +47,9 @@ const ParkingLotForm: React.FC = () => {
     initialValues: {
       title: '',
       description: '',
+      province: '',
+      region: '',
+      city: '',
       //gpstrack: undefined,
     },
 
@@ -33,7 +60,7 @@ const ParkingLotForm: React.FC = () => {
   });
 
   const handleSubmit = async (values: Fields) => {
-    
+
     addHut(values)
 
   }
@@ -41,14 +68,19 @@ const ParkingLotForm: React.FC = () => {
   const addHut = async (values: Fields) => {
     try {
       console.log(values);
-      /*const res = await axios.post('http://localhost:3001/parkinglot', values, QUI AGGIUNGI NUOVO PARKINGLOT
-        {
-          withCredentials: true,
-          headers: {
-            'Content-Type': 'multipart/form-data'
-          }
-        })*/
-         navigate('/');
+      const res = await API.point.createPoint({
+        City: values.city,
+        Latitude: values.latitude,
+        Longitude: values.longitude,
+        Province: values.province,
+        Region: values.region,
+        Label: values.title,
+        ParkingLot: {
+          Description: values.description,
+        }
+      });
+      console.log(res);
+      navigate('/');
 
     } catch (err) {
       setError('Error - creating a new parking lot');
@@ -82,6 +114,46 @@ const ParkingLotForm: React.FC = () => {
               label="Description"
               placeholder="Description of the hut"
               {...form.getInputProps('description')} />
+
+            {display_name && <Paper p={'sm'} withBorder mt={'md'}>
+              <Group>
+                <CiLocationOn style={{ verticalAlign: 'middle' }} size='2em' />
+                <Text fz='lg'>{display_name}</Text>
+              </Group>
+            </Paper>}
+
+            <Box h='300px' mt={'md'}>
+              <MapContainer center={[41.3, 12.5]} zoom={9} className={s.map} >
+
+                <TileLayer
+                  url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                />
+                <EventHandler choosePosition={choosePosition} />
+                {marker && <Marker position={marker} />}
+              </MapContainer>
+            </Box>
+
+            <TextInput hidden
+              {...form.getInputProps('latitude')}
+            />
+
+            <TextInput hidden
+              {...form.getInputProps('longitude')}
+            />
+
+            <TextInput hidden
+              {...form.getInputProps('province')}
+            />
+
+            <TextInput hidden
+              {...form.getInputProps('region')}
+            />
+
+            <TextInput hidden
+              {...form.getInputProps('city')}
+            />
+
+
             <Group position="center">
               <Button mt="xl" type='submit'>Add Hut</Button>
               <Link to="/userarea">
@@ -94,5 +166,18 @@ const ParkingLotForm: React.FC = () => {
     </Container>
   )
 }
+
+interface EventHandlerProps {
+  choosePosition: (marker: LatLng) => void;
+}
+
+const EventHandler = (props: EventHandlerProps) => {
+  const { choosePosition } = props;
+  const map = useMapEvent('click', (e) => {
+    choosePosition(e.latlng);
+  })
+  return null
+}
+
 
 export default ParkingLotForm;
