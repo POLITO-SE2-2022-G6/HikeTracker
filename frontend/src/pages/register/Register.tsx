337d47001600e2342bf5eb0b5948c24809befae5
Francesco Bruno <b.frenc@hotmail.it>
2022-11-27 16:44:02 +0100

    [Frontend] Fix user registration roles

--- frontend/src/pages/register/Register.tsx
+++ frontend/src/pages/register/Register.tsx
@@ -58,8 +58,8 @@ const Register: React.FC = () => {
             label="Type"
             placeholder="Pick one"
             data={[
-              { value: 'Hiker', label: 'Hiker' },
-              { value: 'Guide', label: 'Guide' },
+              { value: 'hiker', label: 'Hiker' },
+              { value: 'guide', label: 'Guide' },
 
             ]}
             {...form.getInputProps('type')}
