30ee44203727701938e9d72185f14f0ce62d7a25
Francesco Bruno <b.frenc@hotmail.it>
2022-12-10 19:16:41 +0100

    [Frontend] Implement Hiker API

---
+++ frontend/src/utilities/api/hikerApi.ts
@@ -0,0 +1,32 @@
+import { Methods } from "./client";
+import { Resource } from "./resource";
+
+export class HikerApi extends Resource {
+  protected path = "/hiker";
+
+  async getPerformance() {
+    return this.client.request({
+      method: Methods.GET,
+      path: `${this.path}/performance`,
+    });
+  }
+
+  async editPerformance(performance: Partial<Performance>) {
+    return this.client.request({
+      method: Methods.PUT,
+      path: `${this.path}/performance`,
+      body: performance,
+    });
+  }
+
+  async getProfile() {
+    // TODO: implement
+  }
+
+  async getRecommendedHikes() {
+    return this.client.request({
+      method: Methods.GET,
+      path: `${this.path}/hikesByPerf`,
+    });
+  }
+}
